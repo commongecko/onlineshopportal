a7974f560f9ef23897f9445a81caa21cc0417da1
Shashank R <shashankr@airmail.cc>
2015-08-12 00:02:17 +0530

    Added preliminary models Items, Basket, Transactions, Customers and added osp to installed apps under settings.py

--- osp/models.py
+++ osp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Items(models.Model):
+    name = models.CharField(max_length=50)
+    cost = models.IntegerField()
+    discount = models.SmallIntegerField(default=0)
+    available = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.name
+
+class Basket(models.Model):
+    totalbill = models.IntegerField()
+
+class Transactions(models.Model):
+    pass
+
+class Customers(models.Model):
+    basket = models.ForeignKey(Basket)
+    history = models.ForeignKey(Transactions)
